d3fba42271398899e72e95e38d32831283614784
Ilya Khayarov <ilya.ssb.ru@mail.ru>
2019-03-05 21:51:02 +0500

    1.3. JavaDoc. Code convention[#117396]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,16 @@
   package ru.job4j.calculate;
  
+ /**
+ * Class Calculate решение задачи части 001 урок 1 
+ * @author khayarov
+ * @since 05.03.2019
+ * @version 1 
+ */
 public class Calculate {
+	/**
+	* Main.
+	* @param arg - arg.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
