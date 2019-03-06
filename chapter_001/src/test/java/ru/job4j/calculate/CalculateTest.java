1c69a30fd6b4e001e51d2d6fde72667e61b90b8a
Ilya Khayarov <ilya.ssb.ru@mail.ru>
2019-03-06 18:53:07 +0500

    2.4. Использование Git в IDEA[#117353]

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -1,10 +1,11 @@
-  package ru.job4j.calculate;
-  
-  import org.junit.Test;
-  import static org.hamcrest.core.Is.is;
-  import static org.junit.Assert.assertThat;
- 
- /**
+package ru.job4j.calculate;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
  * Test.
  *
  * @author Ilya Khayarov (ilya.ssb.ru@mail.ru)
@@ -12,16 +13,16 @@
  * @since 0.1
  */
 public class CalculateTest {
-	/**
-	* Test echo.
-	*/
-	@Test
-	public void whenTakeNameThenThreeEchoPlusName() {
-		String input = "Ilya Khayarov";
-		String expect = "Echo, echo, echo : Ilya Khayarov";
-		Calculate calc = new Calculate();
-		String result = calc.echo(input);
-		assertThat(result, is(expect));
-	}
-	
-	}
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenThreeEchoPlusName() {
+        String input = "Ilya Khayarov";
+        String expect = "Echo, echo, echo : Ilya Khayarov";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
+}
