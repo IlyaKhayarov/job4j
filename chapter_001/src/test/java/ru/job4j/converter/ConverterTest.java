0858d912d10d51f22a423ac8477baf00179a507a
Ilya Khayarov <ilya.ssb.ru@mail.ru>
2019-03-18 12:16:11 +0500

    3.2. Конвертер валюты.[#117384]

---
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Ilya Khayarov (ilya.ssb.ru@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ConverterTest {
+    /**
+     * Test Ruble To Euro.
+     */
+    @Test
+    public void When70RubleToEuroThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToEuro(70);
+        assertThat(result, is(1));
+    }
+
+    /**
+     * Test Ruble To Dollar.
+     */
+    @Test
+    public void When60RubleToDollarThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToDollar(60);
+        assertThat(result, is(1));
+    }
+
+    /**
+     * Test Dollar To Ruble.
+     */
+    @Test
+    public void When1DollarToRubleThen60() {
+        Converter converter = new Converter();
+        int result = converter.dollarToRuble(1);
+        assertThat(result, is(60));
+    }
+
+    /**
+     * Test Euro To Ruble.
+     */
+    @Test
+    public void When1EuroToRubleThen70() {
+        Converter converter = new Converter();
+        int result = converter.euroToRuble(1);
+        assertThat(result, is(70));
+    }
+
+}
