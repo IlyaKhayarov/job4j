18dbacd8a1e60fca64a1ed3b3b553d581df752f0
Ilya Khayarov <ilya.ssb.ru@mail.ru>
2019-03-18 11:50:11 +0500

    3.1. Элементарный калькулятор.[#117387]
    This reverts commit 4dbc6bf4

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,39 +2,15 @@ package ru.job4j.calculator;
 
 public class Calculator {
 
-    /**
-     * Сложение
-     * @param first первый аргумент
-     * @param second второй аргумент
-     * @return результат
-     */
     public double add(double first, double second) {
         return first + second;
     }
-    /**
-     * Вычитание
-     * @param first первый аргумент
-     * @param second второй аргумент
-     * @return результат
-     */
     public double subtract(double first, double second) {
         return first - second;
     }
-    /**
-     * Деление
-     * @param first первый аргумент
-     * @param second второй аргумент
-     * @return результат
-     */
     public double div(double first, double second) {
         return first / second;
     }
-    /**
-     * Умножение
-     * @param first первый аргумент
-     * @param second второй аргумент
-     * @return результат
-     */
     public double multiple(double first, double second) {
         return first * second;
     }
