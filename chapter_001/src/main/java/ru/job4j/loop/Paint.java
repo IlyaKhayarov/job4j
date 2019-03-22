4a971d03e5c8a049e5fb8f19fa8841fefef4dcd2
Ilya Khayarov <ilya.ssb.ru@mail.ru>
2019-03-22 16:25:41 +0500

    5.4. Построить пирамиду в псевдографике.[#117370]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,33 @@
+package ru.job4j.loop;
+
+/**
+ * Пирамида в псевдографике.
+ *
+ * @author Ilya Khayarov (ilya.ssb.ru@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Paint {
+    /**
+     * Пирамида.
+     *
+     * @param height Высота пирамиды.
+     * @return Возвращает строку, представляющую данные в этой строке.
+     */
+    public String pyramid(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = 2 * height - 1;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= height - column - 1 && row + height - 1 >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import java.util.StringJoiner;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Пирамида в псевдографике.
+ *
+ * @author Ilya Khayarov (ilya.ssb.ru@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class PaintTest {
+    /**
+     * Test Пирамида высотой 4.
+     */
+    @Test
+    public void whenPyramid4() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
+                                .toString()
+                )
+        );
+    }
+}
