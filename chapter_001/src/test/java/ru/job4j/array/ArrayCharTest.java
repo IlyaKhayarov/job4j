c0f1811bb1cd303ca9880eb43173903e43d17759
Ilya Khayarov <ilya.ssb.ru@mail.ru>
2019-03-26 12:18:43 +0500

    6.4. Слово начинается с ...[#117376]

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Слово начинается с...
+ *
+ * @author Ilya Khayarov (ilya.ssb.ru@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ArrayCharTest {
+    /**
+     * Test префикс совпадает с словом.
+     */
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("He");
+        assertThat(result, is(true));
+    }
+
+    /**
+     * Test префикс не совпадает с словом.
+     */
+    @Test
+    public void whenStartWithPrefixThenFalse() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("Hi");
+        assertThat(result, is(false));
+    }
+}
