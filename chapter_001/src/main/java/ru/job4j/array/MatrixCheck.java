f37b663893c8b5bbad49db5225054400b4f9310e
Ilya Khayarov <ilya.ssb.ru@mail.ru>
2019-03-29 10:48:27 +0500

    6.7. Квадратный массив заполнен true или false по диагоналям.[#117378]

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+/**
+ * Квадратный массив заполнен true или false по диагоналям.
+ *
+ * @author Ilya Khayarov (ilya.ssb.ru@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class MatrixCheck {
+    /**
+     * Квадратный массив заполнен true или false по диагоналям.
+     *
+     * @param data Массив.
+     * @return True или false.
+     */
+    public boolean mono(boolean[][] data) {
+        boolean result = true;
+        int z = data.length / 2 + 1;
+        boolean b = data[z][z];
+        for (int i = 0; i < data.length; i += 2) {
+            for (int j = 0; j < data.length; j += 2) {
+                if (data[i][j] != b) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Квадратный массив заполнен true или false по диагоналям.
+ *
+ * @author Ilya Khayarov (ilya.ssb.ru@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class MatrixCheckTest {
+    /**
+     * Test true.
+     */
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][]{
+                {true, true, true},
+                {false, true, true},
+                {true, false, true},
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    /**
+     * Test false.
+     */
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][]{
+                {true, true, false},
+                {false, false, true},
+                {true, false, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+}
