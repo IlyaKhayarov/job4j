7ecb6efc09fca549f5c7c65520846ff5c7d0e4d3
Ilya Khayarov <ilya.ssb.ru@mail.ru>
2019-04-11 12:05:02 +0500

    Объединить два массива

---
+++ chapter_002/src/main/java/Merge/Merge.java
@@ -0,0 +1,25 @@
+package Merge;
+
+/**
+ * Объединить два массива
+ *
+ * @author Ilya Khayarov
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Merge {
+    /**
+     * Объединить два массива
+     *
+     * @param left  Массив 1
+     * @param right Массив 2
+     * @return Массив 3 (объединенный 1 и 2)
+     */
+    public int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        System.arraycopy(left, 0, rsl, 0, left.length);
+        System.arraycopy(right, 0, rsl, left.length, right.length);
+        return rsl;
+    }
+}
---
+++ chapter_002/src/test/java/Merge/MergeTest.java
@@ -0,0 +1,86 @@
+package Merge;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Объединить два массива
+ *
+ * @author Ilya Khayarov
+ * @version $Id$
+ * @since 0.1
+ */
+public class MergeTest {
+    /**
+     * 0х0.
+     */
+    @Test
+    public void whenBothEmpty() {
+        Merge algo = new Merge();
+        int[] expect = new int[0];
+        int[] result = algo.merge(
+                new int[0],
+                new int[0]
+        );
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * 2х2.
+     */
+    @Test
+    public void whenAskOrder() {
+        Merge algo = new Merge();
+        int[] expect = {1, 2, 3, 4};
+        int[] result = algo.merge(
+                new int[]{1, 2},
+                new int[]{3, 4}
+        );
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * 3х2.
+     */
+    @Test
+    public void whenLeftLess() {
+        Merge algo = new Merge();
+        int[] expect = {1, 2, 3, 3, 4};
+        int[] result = algo.merge(
+                new int[]{1, 2, 3},
+                new int[]{3, 4}
+        );
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * 2х3.
+     */
+    @Test
+    public void whenLeftGreat() {
+        Merge algo = new Merge();
+        int[] expect = {1, 2, 3, 4, 4};
+        int[] result = algo.merge(
+                new int[]{1, 2},
+                new int[]{3, 4, 4}
+        );
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * 0х4.
+     */
+    @Test
+    public void whenLeftEmpty() {
+        Merge algo = new Merge();
+        int[] expect = {1, 3, 4, 4};
+        int[] result = algo.merge(
+                new int[]{},
+                new int[]{1, 3, 4, 4}
+        );
+        assertThat(result, is(expect));
+    }
+
+}
