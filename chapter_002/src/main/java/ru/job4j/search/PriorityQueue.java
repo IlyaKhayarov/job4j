24848d157a2f854ccc880ca046196395e04cb361
Ilya Khayarov <ilya.ssb.ru@mail.ru>
2019-09-23 17:46:45 +0500

    2. Очередь с приоритетом на LinkedList[#117420] Fixed.

--- chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -1,15 +1,28 @@
 package ru.job4j.search;
 
-import java.util.Collections;
 import java.util.LinkedList;
 
 public class PriorityQueue {
+
     private LinkedList<Task> tasks = new LinkedList<>();
 
     public void put(Task task) {
-        tasks.add(task);
-        TaskSort taskSort = new TaskSort();
-        Collections.sort(tasks, taskSort);
+        int size = tasks.size();
+        int g = 0;
+        if (size == 0) {
+            tasks.add(task);
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (task.getPriority() < tasks.get(i).getPriority()) {
+                    tasks.add(g, task);
+                    break;
+                } else {
+                    g++;
+                    if (g == size) tasks.add(task);
+                }
+            }
+
+        }
     }
 
     public Task take() {
