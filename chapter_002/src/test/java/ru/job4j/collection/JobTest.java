2a05e7b00bad7d07f4d6447e9206aa95f0e1fd29
Ilya Khayarov <ilya.ssb.ru@mail.ru>
2020-03-16 07:26:54 +0500

    2. Сортировка User с использованием Comparator[#117412] Fix

--- chapter_002/src/test/java/ru/job4j/collection/JobTest.java
+++ chapter_002/src/test/java/ru/job4j/collection/JobTest.java
@@ -10,27 +10,37 @@ import static org.junit.Assert.assertThat;
 
 public class JobTest {
     @Test
-    public void whenCompatorByNameAndProrityDesc() {
+    public void whenComparatorByNameAndProrityDesc() {
         Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 0),
                 new Job("Fix bug", 1)
         );
         assertThat(rsl, lessThan(0));
+        int rsl2 = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Impl task", 1)
+        );
+        assertThat(rsl2, greaterThan(0));
     }
 
     @Test
-    public void whenCompatorByNameAndProrityAsc() {
+    public void whenComparatorByNameAndProrityAsc() {
         Comparator<Job> cmpNamePriority = new JobAscByName().thenComparing(new JobAscByPriority());
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 0),
                 new Job("Fix bug", 1)
         );
         assertThat(rsl, greaterThan(0));
+        int rsl2 = cmpNamePriority.compare(
+                new Job("Impl task", 1),
+                new Job("Impl task", 0)
+        );
+        assertThat(rsl2, greaterThan(0));
     }
 
     @Test
-    public void whenCompatorByNameAsc() {
+    public void whenComparatorByNameAsc() {
         Comparator<Job> cmpNamePriority = new JobAscByName();
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 0),
@@ -40,7 +50,7 @@ public class JobTest {
     }
 
     @Test
-    public void whenCompatorByPriorityAsc() {
+    public void whenComparatorByPriorityAsc() {
         Comparator<Job> cmpNamePriority = new JobAscByPriority();
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 0),
@@ -50,7 +60,7 @@ public class JobTest {
     }
 
     @Test
-    public void whenCompatorByNameDesc() {
+    public void whenComparatorByNameDesc() {
         Comparator<Job> cmpNamePriority = new JobDescByName();
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 0),
@@ -60,7 +70,7 @@ public class JobTest {
     }
 
     @Test
-    public void whenCompatorByPriorityDesc() {
+    public void whenComparatorByPriorityDesc() {
         Comparator<Job> cmpNamePriority = new JobDescByPriority();
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 0),
